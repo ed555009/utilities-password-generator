464f398bb81a22bfb0e9659921cb7fd82093da29
Ed Wang <ed555009@gmail.com>
2024-05-17 18:58:27 +0800

    initial commit

---
+++ src/Utilities.PasswordGenerator/Interfaces/IPasswordGeneratorService.cs
@@ -0,0 +1,25 @@
+namespace Utilities.PasswordGenerator.Interfaces
+{
+	/// <summary>
+	/// Represents a service for generating passwords.
+	/// </summary>
+	public interface IPasswordGeneratorService
+	{
+		/// <summary>
+		/// Generates a password with the specified requirements.
+		/// </summary>
+		/// <param name="length">The length of the password.</param>
+		/// <param name="requiredUppercase">The minimum number of uppercase characters required in the password.</param>
+		/// <param name="requiredLowercase">The minimum number of lowercase characters required in the password.</param>
+		/// <param name="requiredNumeric">The minimum number of numeric characters required in the password.</param>
+		/// <param name="requiredSpecialChar">The minimum number of special characters required in the password.</param>
+		/// <param name="specialChars">The special characters allowed in the password.</param>
+		/// <returns>A randomly generated password.</returns>
+		string Generate(int length = 8,
+			int requiredUppercase = 1,
+			int requiredLowercase = 1,
+			int requiredNumeric = 1,
+			int requiredSpecialChar = 1,
+			string specialChars = "!@#$%^&*()-_=+<>?");
+	}
+}
---
+++ test/Utilities.PasswordGenerator.Tests/PasswordGeneratorServiceTests.cs
@@ -0,0 +1,58 @@
+using Utilities.PasswordGenerator.Services;
+using Xunit.Abstractions;
+
+namespace Utilities.PasswordGenerator.Tests;
+
+public class PasswordGeneratorServiceTests
+{
+	private readonly ITestOutputHelper _outputHelper;
+
+	public PasswordGeneratorServiceTests(ITestOutputHelper outputHelper) =>
+		_outputHelper = outputHelper;
+
+	[Theory]
+	[InlineData(8)]
+	[InlineData(16)]
+	[InlineData(32)]
+	public void Generate_ShouldSuccess(int length)
+	{
+		// Given
+		var service = new PasswordGeneratorService();
+
+		// When
+		var result = service.Generate(length);
+
+		_outputHelper.WriteLine(result);
+
+		// Then
+		Assert.Equal(length, result.Length);
+	}
+
+	[Theory]
+	[InlineData(7)]
+	[InlineData(33)]
+	public void Generate_WithLengthOutOfRange_ShouldThrow(int length)
+	{
+		// Given
+		var service = new PasswordGeneratorService();
+
+		// When
+		var ex = Assert.Throws<ArgumentOutOfRangeException>(() => service.Generate(length));
+
+		// Then
+		Assert.NotNull(ex);
+	}
+
+	[Fact]
+	public void Generate_WithLengthLessThanRequired_ShouldThrow()
+	{
+		// Given
+		var service = new PasswordGeneratorService();
+
+		// When
+		var ex = Assert.Throws<ArgumentException>(() => service.Generate(8, 3, 3, 3, 3));
+
+		// Then
+		Assert.NotNull(ex);
+	}
+}
